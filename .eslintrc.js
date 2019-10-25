3b9360349375363180f8bd2f1c1460c609a4b02a
Harshad patel <harshad@aspireedge.com>
2019-10-25 11:05:44 +0530

    added code for react on rails app

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'airbnb',
+  "globals": {
+    "window": true,
+    "document": true
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-console': 0,
+    'no-alert': 0,
+  },
+};
